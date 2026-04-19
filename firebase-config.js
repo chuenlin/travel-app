0afb861fac45af7aba5d9420e3e3c178d88ee8a2
chuen714 <chuen.lin@dynasafe.com.tw>
2026-04-19 20:25:44 +0800

    add firebase config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 設定
+window.firebaseConfig = {
+  apiKey:            "AIzaSyATLyK11BRSzw5w538KNgSiyE948u2TSdI",
+  authDomain:        "travel-app-b0d83.firebaseapp.com",
+  databaseURL:       "https://travel-app-b0d83-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId:         "travel-app-b0d83",
+  storageBucket:     "travel-app-b0d83.firebasestorage.app",
+  messagingSenderId: "19423957658",
+  appId:             "1:19423957658:web:8404fae8bac1ee5f15c523"
+};
